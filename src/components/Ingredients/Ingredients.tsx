4fc11135f579590aed0bb305986f02ac7469ecb9
enes-dogan <enesdogan210@gmail.com>
2023-08-26 17:30:00 +0300

    implementing search feature (works if the whole input equals to the title)

--- src/components/Ingredients/Ingredients.tsx
+++ src/components/Ingredients/Ingredients.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 
 import { Ingredient } from '../../types';
 
@@ -8,26 +8,27 @@ import IngredientList from './IngredientList';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState<Ingredient[]>([]);
+  
+  //? Unnecessary because we make a initial fetch in <Search /> component already if there is no query
+  // useEffect(() => {// useEffect() gets executed after component render
+  //   fetch('https://react-hooks-45f20-default-rtdb.firebaseio.com/ingredients.json').then(response => response.json()
+  //   ).then(responseData => { // responseData is an object because of firebase returns as an object
+  //     const loadedIngredients = [];
+  //     for (const key in responseData) {
+  //       loadedIngredients.push({
+  //         id: key,
+  //         title: responseData[key].ingredient.title,
+  //         amount: responseData[key].ingredient.amount,
+  //       });
+  //     }
+  //     setUserIngredients(loadedIngredients);
+  //   })
+  // }, []);
 
-  useEffect(() => {// useEffect() gets executed after component render
-    fetch('https://react-hooks-45f20-default-rtdb.firebaseio.com/ingredients.json').then(response => response.json()
-    ).then(responseData => { // responseData is an object because of firebase returns as an object
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].ingredient.title,
-          amount: responseData[key].ingredient.amount,
-        });
-      }
-      setUserIngredients(loadedIngredients);
-    })
-  }, []);
-
-  const filteredIngredientsHandler = (filteredIngredients: Ingredient[]) => {
+  const filteredIngredientsHandler = useCallback((filteredIngredients: Ingredient[]) => {
     setUserIngredients(filteredIngredients);
-  }
-
+  }, []);
+  // useCallback() is caches the function passed as an argument and it survives re-renders thus function not re-created
   const addIngredientHandler = (ingredient: { title: string; amount: string }) => {
     fetch('https://react-hooks-45f20-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
--- src/components/Ingredients/Search.tsx
+++ src/components/Ingredients/Search.tsx
@@ -8,7 +8,11 @@ import './Search.css';
 const Search = React.memo(({ onLoadedIngredients }: SearchProps) => {
   const [enteredFilter, setEnteredFilter] = useState('');
   useEffect(() => {
-    fetch('https://react-hooks-45f20-default-rtdb.firebaseio.com/ingredients.json').then(response => response.json()
+    const query = 
+    enteredFilter.length === 0 ? '' 
+    : `?orderBy="ingredient/title"&equalTo="${enteredFilter}"`;
+    fetch('https://react-hooks-45f20-default-rtdb.firebaseio.com/ingredients.json' + query)
+    .then(response => response.json()
     ).then(responseData => {
       const loadedIngredients = [];
       for (const key in responseData) {
@@ -17,10 +21,11 @@ const Search = React.memo(({ onLoadedIngredients }: SearchProps) => {
           title: responseData[key].ingredient.title,
           amount: responseData[key].ingredient.amount,
         });
-      }
-      onLoadedIngredients(loadedIngredients)
-    });
-  }, [onLoadedIngredients, enteredFilter]);
+      }// function passed into onLoadedIngredients (filteredIngredientsHandler) is
+      // re-created because <Search/> component re-created form scratch causes infinite loop
+      onLoadedIngredients(loadedIngredients) 
+    }); // We should be explicit about our useEffect() dependencies
+  }, [onLoadedIngredients, enteredFilter]); // We only want to run this func when onLoadedIngregients changes/executes
 
   return (
     <section className="search">
