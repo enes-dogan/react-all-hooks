62380a07d88d1d5da6161f328fc815f5dc59c5cd
enes-dogan <enesdogan210@gmail.com>
2023-09-18 23:57:04 +0300

    fixing auth context

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const AuthContext = React.createContext({
-  status: false, login: () => {
-    isAuth: false,
-    login: () => { }
-  }
-})
+  isAuth: false,
+  login: () => { }
+});
 
-const AuthContextProvider = (props) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+const AuthContextProvider = props => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const loginHandler = () => {
-    setIsAuthenticated(true)
-  }
+    setIsAuthenticated(true);
+  };
 
   return (
-    <AuthContext.Provider value={{ login: loginHandler, status: isAuthenticated }}>
+    <AuthContext.Provider
+      value={{ login: loginHandler, isAuth: isAuthenticated }}
+    >
       {props.children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
+
+export default AuthContextProvider;
